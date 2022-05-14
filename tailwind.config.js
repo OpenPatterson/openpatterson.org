d398556c00137b08d849090fadaf4eb38db83be1
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2022-05-13 23:31:16 -0700

    Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1140px'
+    },
+    extend: {
+      colors: { 
+        'neutral-off-white': '#efefef', 
+        'rose-red': '#db2b39', 
+        'navy-blue': '#29335c', 
+        'primary': '#ffcf99',
+        'primary-light': '#FFE1BE', 
+        'neutral-off-black': '#1C211E'
+      }
+    },
   },
   plugins: [],
 }
