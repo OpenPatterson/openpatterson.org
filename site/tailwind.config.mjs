66b9e847c9915f4f70872989f9594d9aa44f9093
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-10-29 01:05:51 -0700

    Made header component in all skeleton pages

--- site/tailwind.config.mjs
+++ site/tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'primary' : 'hsla(35, 100%, 55%, 1)',
+				'primary-light': 'hsla(32, 95%, 77%, 1)',
+				'primary-super-light': 'hsla(32, 95%, 77%, 0.5)',
+			},
+		},
 	},
 	plugins: [],
 }
