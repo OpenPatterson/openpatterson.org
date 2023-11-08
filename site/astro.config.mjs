5fff09653e19182c81568b12bbf1c141b8e40c03
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-11-07 21:36:32 -0800

    Update astro.config.mjs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import mdx from "@astrojs/mdx";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "hybrid",
