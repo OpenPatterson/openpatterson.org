bf80c4d2ffb8359768630f5af10fcec76546e4d7
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2023-11-09 14:20:17 -0800

    Adding vercel analytics

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
@@ -8,6 +8,10 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   integrations: [tailwind(), react(), mdx()],
   output: "hybrid",
-  adapter: vercel(),
-  renderers: ['@astrojs/renderer-react']
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  renderers: ["@astrojs/renderer-react"],
 });
