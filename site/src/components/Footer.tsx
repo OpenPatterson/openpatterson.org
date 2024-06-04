50ff5ae88309b7cb31f7c9e8ad063200b1c94bb2
Carlos Eduardo Ortega <31175269+SoyCarlos@users.noreply.github.com>
2024-06-04 15:07:25 -0700

    Added newsletter sign up functionality

--- site/src/components/Footer.tsx
+++ site/src/components/Footer.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 import { Container } from '@/components/Container'
 import { FadeIn } from '@/components/FadeIn'
@@ -78,34 +79,62 @@ function ArrowIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
 }
 
 function NewsletterForm() {
-  return (
-    <form className="max-w-sm">
-      <h2 className="font-display text-sm font-semibold tracking-wider text-neutral-950">
-        Sign up for our newsletter
-      </h2>
-      <p className="mt-4 text-sm text-neutral-700">
-        Subscribe to hear the latest about our projects and receive articles we find around the internet that we love.
-      </p>
-      <div className="relative mt-6">
-        <input
-          type="email"
-          placeholder="Email address"
-          autoComplete="email"
-          aria-label="Email address"
-          className="block w-full rounded-2xl border border-neutral-300 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-neutral-500 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5"
-        />
-        <div className="absolute inset-y-1 right-1 flex justify-end">
-          <button
-            type="submit"
-            aria-label="Submit"
-            className="flex aspect-square h-full items-center justify-center rounded-xl bg-neutral-950 text-white transition hover:bg-neutral-800"
-          >
-            <ArrowIcon className="w-4" />
-          </button>
+  const [email, setEmail] = useState('')
+  const [statusLabel, setStatusLabel] = useState('')
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setStatusLabel('Sending...')
+
+    const response = await fetch('/api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+      }),
+    })
+    const result = await response.json()
+    if (response.ok) {
+      setStatusLabel("Signed Up Successfully");
+    } else {
+      setStatusLabel("Submission failed. Make sure to use valid email");
+      console.error(result.error);
+    }
+  }
+
+    return (
+      <form className="max-w-sm" onSubmit={handleSubmit}>
+        <h2 className="font-display text-sm font-semibold tracking-wider text-neutral-950">
+          Sign up for our newsletter
+        </h2>
+        <p className="mt-4 text-sm text-neutral-700">
+          Subscribe to hear the latest about our projects and receive articles
+          we find around the internet that we love.
+        </p>
+        <div className="relative mt-6">
+          <input
+            type="email"
+            placeholder="Email address"
+            autoComplete="email"
+            aria-label="Email address"
+            className="block w-full rounded-2xl border border-neutral-300 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-neutral-500 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)}
+          />
+          <div className="absolute inset-y-1 right-1 flex justify-end">
+            <button
+              type="submit"
+              aria-label="Submit"
+              className="flex aspect-square h-full items-center justify-center rounded-xl bg-neutral-950 text-white transition hover:bg-neutral-800"
+            >
+              <ArrowIcon className="w-4" />
+            </button>
+          </div>
         </div>
-      </div>
-    </form>
-  )
+        <a href="" className="">{statusLabel}</a>
+      </form>
+    )
 }
 
 export function Footer() {
